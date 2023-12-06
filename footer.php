155bb7054679e7d10f6e1f62b137e35363f9a8f1
scanbeu <136927001+scanbeu@users.noreply.github.com>
2023-12-05 22:15:36 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,43 @@
+<footer>
+        <div class="tab">
+            <h1 class="pop-element">Shop</h1>
+            <div class="subtabs">
+                <a href="Home.html">Home</a>
+                <a href="Product.html">Our Product</a>
+            </div>
+            <h1 class="pop-element">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Company</h1>
+            <div class="subtabs">
+                <a href="about.html">About</a>
+                <a href="Reviews.html">Reviews</a>
+                <a href="FAQ.html">FAQ</a>
+            </div>
+            <h1 class="pop-element">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Contact Us</h1>
+            <div class="subtabs">
+                <p>contact@Polaroid.corp</p>
+                <p>+1-(123)-456-4440</p>
+                <p>111, tupac shakur road, Toronto</p>
+            </div>
+            <h1 class="pop-element">Follow Us</h1>
+            <div class="subtabs">
+                <a href="#">Twitter</a>
+                <a href="#">Facebook</a>
+                <a href="#">Instagram</a>
+                <a href="#">Github</a>
+            </div>
+        </div>
+        <div class="footerdiv">
+            <div class="tab-2">
+                <a href="#">Terms & Conditions</a>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Shipping Policy</a>
+                <a href="#">Refund Policy</a>
+                <a href="#">Cookie Policy</a>
+            </div>
+            <p>© 2023 by Polaroid Camera Corp.</p>
+        </div>
+    </footer>
+    
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FinalProjrctPHP</title>
+    <link rel="stylesheet" href="css/style3.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital@1&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <nav class="navbar">
+            <img class="logo" src="img/polaroid2.png" alt="website Logo">
+            <div class="searchBar">
+                <input type="search" placeholder="Search for music">
+            </div>
+            <div class="Acontainer">
+                <a href="index.php" class="active">Home</a>
+                <a href="signup.php">Register</a>
+                <a href="login.php">Login</a>
+                <a href="">Contact</a>
+            </div>
+        </nav>
+    </header>
